75ad1f760b63585f1a8a618fbbda54eeeda081bd
GolemBebrov <bebrovgolem@gmail.com>
2025-09-01 08:08:38 +0300

    Small compile change

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-# Явно описываем наше расширение
 ext_modules = [
     Extension(
-        # Имя модуля для импорта
         "nevu_ui.fast_logic",
-        # Путь к исходному файлу от корня проекта
-        ["src/nevu_ui/fast_logic.pyx"]
-    )
+        ["src/nevu_ui/fast_logic.pyx"],
+    ),
+    Extension(
+        "nevu_ui.fast_shapes",
+        ["src/nevu_ui/fast_shapes.pyx"],
+    ),
+
 ]
 
 setup(
     ext_modules = cythonize(ext_modules),
-    # Не забываем указать заголовочные файлы NumPy
     include_dirs=[numpy.get_include()]
 )
