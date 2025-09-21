9dfcd517ac8a5582fa16cd42e1fd6b40b6c2f21e
GolemBebrov <bebrovgolem@gmail.com>
2025-09-21 23:02:36 +0300

    Restructurized, force_quit option, new on_start/exit hooks
    - Added: _force_quit flag to manage exit process
    - Added: on_start/exit hooks
    - Changed: restructurized imports for coming 0.5.6 release
    - Changed: _a_system_*, _b_system_* fucn to _after_*, _before_*
    - Changed: sorted update/draw function for better readability
    - Fixed: removed unused imports
    - Fixed: small performance bug with dirty_rects

--- src/nevu_ui/ui_manager.py
+++ src/nevu_ui/ui_manager.py
@@ -1,68 +1,85 @@
-from .window import Window
-from .utils import *
 import pygame
 import sys
+
+from .window import Window
+
 class Manager:
-    def __init__(self, window: Window = None):
+    def __init__(self, window: Window | None = None):
         if window: self.window = window
         self.running = True
+        self._dirty_mode = True
+        self._force_quit = True
         self.background = (0, 0, 0)
         self.fps = 60
-        
-        self._dirty_mode = True
+
     @property
     def window(self):
         return self._window
     @window.setter
     def window(self, window: Window):
         if not isinstance(window, Window):
-            raise TypeError("window must be a Window object")
+            raise ValueError("Unexpected window type!")
         self._window = window
+
+    def _before_draw_loop(self):
+        self.window.clear(self.background)
     def draw_loop(self):
         pass
-    def _a_system_draw_loop(self):
+    def _after_draw_loop(self):
         pass
-    def exit(self):
-        self.running = False
-    def _b_system_draw_loop(self):
-        self.window.clear(self.background)
+
     def __main_draw_loop(self):
-        self._b_system_draw_loop()
+        self._before_draw_loop()
         self.draw_loop()
-        self._a_system_draw_loop()
+        self._after_draw_loop()
+
+    def _before_update_loop(self, events):
+        pass
     def update_loop(self, events):
         pass
-    def _a_system_update_loop(self, events):
+    def _after_update_loop(self, events):
         self.window.update(events, self.fps)
-    def _b_system_update_loop(self, events):
-        pass
+
     def __main_update_loop(self):
         events = pygame.event.get()
-        self._b_system_update_loop(events)
+        self._before_update_loop(events)
         self.update_loop(events)
-        self._a_system_update_loop(events)
+        self._after_update_loop(events)
+    
+    def exit(self):
+        self.running = False
+    
+    def on_start(self):
+        pass
+    
     def on_exit(self):
-        pygame.quit()
-        sys.exit()
+        pass 
+    
+    def _on_exit(self):
+        self.on_exit()
+        if self._force_quit:
+            pygame.quit()
+            sys.exit()
+        
     def first_update(self):
         pass
+    
     def first_draw(self):
         pass
+    
     def __main_loop(self):
+        self.on_start()
         self.first_update()
         self.first_draw()
         while self.running:
             self.__main_update_loop()
             self.__main_draw_loop()
             if self._dirty_mode:
-                #print(self.window._next_update_dirty_rects)
                 pygame.display.update(self.window._next_update_dirty_rects)
-                self.window._next_update_dirty_rects = []
             else:
-                self.window._next_update_dirty_rects = []
                 pygame.display.update()
-        self.on_exit()
+            self.window._next_update_dirty_rects = []
+        self._on_exit()
+        
     def run(self):
         self.__main_loop()
-
-        
