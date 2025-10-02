c3ee76c272a0af4e3bd1c58bbd834eade9604b77
GolemBebrov <bebrovgolem@gmail.com>
2025-10-02 23:40:54 +0300

    __init__ update

--- src/nevu_ui/widgets/__init__.py
+++ src/nevu_ui/widgets/__init__.py
@@ -1,12 +1,13 @@
 from .widget import Widget
 from .label import Label
 from .button import Button
-from .empty_widget import Empty_Widget
+from .empty_widget import EmptyWidget
 from .rect_checkbox import RectCheckBox
 from .image import Image
 from .gif import Gif
 from .input import Input
 from .music_player import MusicPlayer
+from .element_switcher import ElementSwitcher, Element
 __all__ = [
-    'Widget', 'Label', 'Button', 'Empty_Widget', 'RectCheckBox', 'Image', 'Gif', 'Input', 'MusicPlayer'
+    'Widget', 'Label', 'Button', 'EmptyWidget', 'RectCheckBox', 'Image', 'Gif', 'Input', 'MusicPlayer', 'ElementSwitcher', 'Element'
 ]
