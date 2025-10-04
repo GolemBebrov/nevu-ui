8dd8311f4346310ebc80370e2e2b9f42ca5a8fb3
GolemBebrov <bebrovgolem@gmail.com>
2025-10-04 15:08:32 +0300

    Renewed ProgressBar.
    - ProgressBar: widget for displaying the progress of an operation or a value within a defined range.
    
    - Added: inline support
    - Added: small docs
    - Changed: all api to the modern version of Nevu UI
    - Changed: adjusted for better support of ColorTheme

--- src/nevu_ui/widgets/progress_bar.py
+++ src/nevu_ui/widgets/progress_bar.py
@@ -2,6 +2,7 @@ import copy
 
 from nevu_ui.fast.nvvector2 import NvVector2
 from nevu_ui.widgets import Widget
+from nevu_ui.color import PairColorRole
 
 from nevu_ui.style import (
     Style, default_style
@@ -11,33 +12,89 @@ class ProgressBar(Widget):
     min_value: int | float
     max_value: int | float
     _current_value: int | float
-    def __init__(self, size: NvVector2 | list, style: Style = default_style,**constant_kwargs):
+    color_pair_role: PairColorRole
+    def __init__(self, size: NvVector2 | list, style: Style = default_style, **constant_kwargs):
+        """Initializes a new ProgressBar widget.
+
+        A visual widget that indicates the progress of an operation or a value
+        within a defined range. The filled portion of the bar represents the
+        current value relative to its minimum and maximum bounds.
+
+        Parameters
+        ----------
+        size : NvVector2 | list
+            The size of the widget.
+        style : Style, optional
+            The style object for the widget. Defaults to default_style.
+        min_value : int or float, optional
+            The minimum value of the progress bar. Alias: 'min'.
+            Passed via keyword arguments. Defaults to 0.
+        max_value : int or float, optional
+            The maximum value of the progress bar. Alias: 'max'.
+            Passed via keyword arguments. Defaults to 100.
+        value : int or float, optional
+            The initial value of the progress bar. This will determine the
+            initial filled percentage. Passed via keyword arguments.
+            Defaults to 0.
+        color_pair_role : PairColorRole, optional
+            The color role used to determine the color of the filled progress
+            portion. Alias: 'role'. Passed via keyword arguments.
+            Defaults to PairColorRole.BACKGROUND.
+        """
         super().__init__(size, style, **constant_kwargs)
-        self.percentage_of_value = self.value
+        self.set_progress_by_value(self.value)
+    
+    def _init_booleans(self):
+        super()._init_booleans()
+        self.hoverable = False
+    
     def _add_constants(self):
         super()._add_constants()
         self._add_constant("min_value", (int, float), 0)
+        self._add_constant_link("min", "min_value")
         self._add_constant("max_value", (int, float), 100)
-        self._add_constant("value", (int, float), 0)
-    @property
-    def percentage(self):
-        return self._percentage
-    @percentage.setter
-    def percentage(self,value):
-        self._percentage = value
-        self.value = self.min_value+(self.max_value-self.min_value)*self._percentage
-    @percentage.setter
-    def percentage_of_value(self,value):
-        self._percentage = (value-self.min_value)/(self.max_value-self.min_value)
-    @property
-    def value(self):
-        return self._current_value
-    @value.setter
-    def value(self,value):
-        self._current_value = value
-        self.percentage_of_value = value
-    def secondary_draw(self):
-        super().secondary_draw()
-        surf = self.renderer._create_surf_base([int(self.relx(self.size[0]*self.percentage))-2,int(self.rely(self.size[1]))-2])
-        self.surface.blit(surf,(0,0))
+        self._add_constant_link("max", "max_value")
+        self._add_constant("value", (int, float), 0, getter=self.value_getter, setter=self.value_setter)
+        self._add_constant("color_pair_role", PairColorRole, PairColorRole.BACKGROUND)
+        self._add_constant_link("role", "color_pair_role")
         
+    @property
+    def progress(self): return self._progress
+    
+    @progress.setter
+    def progress(self, value):
+        self._progress = value
+        self.value = self.min_value + (self.max_value - self.min_value) * self._progress
+        
+    def set_progress_by_value(self, value: int | float):
+        self._progress = (value - self.min_value) / (self.max_value - self.min_value)
+        
+    def value_getter(self): return self._current_value
+    def value_setter(self, value): 
+        self._current_value = value
+        self._changed = True
+        if not hasattr(self, "_progress"):
+            self.set_progress_by_value(value)
+    
+    def _init_alt(self):
+        super()._init_alt()
+        self._subtheme_progress = self._alt_subtheme_progress if self.alt else self._main_subtheme_progress
+    
+    @property
+    def _main_subtheme_progress(self):
+        return self.style.colortheme.get_pair(self.color_pair_role).color
+
+    @property
+    def _alt_subtheme_progress(self):
+        return self.style.colortheme.get_pair(self.color_pair_role).oncolor
+    
+    def secondary_draw_content(self):
+        super().secondary_draw_content()
+        if self._changed:
+            surf = self.renderer._create_surf_base(NvVector2(self._rsize.x * self.progress, self._rsize.y), override_color = self._subtheme_progress, radius = round(self.relm(self.style.borderradius)))
+            coords = self.coordinates if self.inline else NvVector2(0, 0)
+            coords += (self._rsize_marg / 2).to_round()
+            self.surface.blit(surf, coords)
+    
+    def clone(self):
+        return ProgressBar(self._lazy_kwargs['size'], copy.deepcopy(self.style), **self.constant_kwargs)
