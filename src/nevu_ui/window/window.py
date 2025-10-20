b4b39305b912fef912b077c4ab8d48146f97c095
GolemBebrov <bebrovgolem@gmail.com>
2025-10-20 17:11:43 +0300

    changed: init structure, added: nevu_state integration, two renderers support, unified api

--- src/nevu_ui/window/window.py
+++ src/nevu_ui/window/window.py
@@ -1,8 +1,13 @@
 import sys
 import pygame
-
+from pygame._sdl2 import Renderer
 from nevu_ui.fast.nvvector2 import NvVector2
+from warnings import deprecated
 
+from nevu_ui.state import nevu_state
+from nevu_ui.window.display import (
+    DisplayClassic, DisplaySdl, DisplayBase
+)
 from nevu_ui.core_types import (
     ResizeType, EventType
 )
@@ -12,7 +17,10 @@ from nevu_ui.utils import (
 from nevu_ui.fast.zsystem import (
     ZSystem, ZRequest
 )
+
+
 class Window:
+    _display: DisplayBase
     @staticmethod
     def cropToRatio(width: int, height: int, ratio: NvVector2, default=(0, 0)):
         if height == 0 or ratio.y == 0: return default
@@ -26,32 +34,22 @@ class Window:
         else:
             crop_height = height - (width * ry / rx)
             return default[0], crop_height
+    
+    def __init__(self, size, minsize=(10, 10), title="pygame window", resizable = True, ratio: NvVector2 | None = None, resize_type: ResizeType = ResizeType.CropToRatio, _gpu_mode = False):
+        self._gpu_mode = _gpu_mode
 
-    def __init__(self, size, minsize=(10, 10), title="pygame window", resizable=True, ratio: NvVector2 | None = None, resize_type: ResizeType = ResizeType.CropToRatio):
-        self._original_size = tuple(size)
-        self.size = list(size)
-        self.minsize = list(minsize)
         self.resize_type = resize_type
         
-        flags = pygame.RESIZABLE if resizable else 0
-        flags |= pygame.HWSURFACE | pygame.DOUBLEBUF
-        if self.resize_type == ResizeType.ResizeFromOriginal:
-            flags |= pygame.SCALED
-        
-        self.surface = pygame.display.set_mode(size, flags=flags)
-        
+        self.resizable = resizable
         self._title = title
-        pygame.display.set_caption(self._title)
         
-        self._ratio = ratio or NvVector2(0, 0)
+        self._init_lists(ratio, size, minsize)
+        self._init_graphics()
+        
         self._clock = pygame.time.Clock()
-        
         self._events: list[NevuEvent] = []
-        self.last_events = []
-        
-        self._crop_width_offset = 0
-        self._crop_height_offset = 0
-        self._offset = NvVector2(0, 0)
+        nevu_state.current_events = []
+
         if self.resize_type == ResizeType.CropToRatio:
             self._recalculate_render_area() 
 
@@ -60,16 +58,54 @@ class Window:
         
         self.z_system = ZSystem()
         
+        self._set_nevu_state()
+
+    def _set_nevu_state(self):
+        nevu_state.window = self
+        if self._gpu_mode:
+            assert isinstance(self._display, DisplaySdl)
+            nevu_state.renderer = self._display.renderer
+        nevu_state.z_system = self.z_system
+        
+    def _init_lists(self, ratio, size, minsize):
+        self._ratio = ratio or NvVector2(0, 0)
+        self._original_size = NvVector2(size)
+        self.size = NvVector2(size)
+        self.minsize = NvVector2(minsize)
+        
+        self._crop_width_offset = 0
+        self._crop_height_offset = 0
+        self._offset = NvVector2(0, 0)
+    def _init_graphics(self):
+        if not self._gpu_mode:
+            flags = pygame.RESIZABLE if self.resizable else 0
+            flags |= pygame.HWSURFACE | pygame.DOUBLEBUF
+            self._display = DisplayClassic(self.title, self.size.to_tuple(), flags)
+        else:
+            kwargs = {}
+            if self.resizable:
+                kwargs['resizable'] = True
+            self._display = DisplaySdl(self.title, self.size.to_tuple(), **kwargs)
+        
+    @property
+    @deprecated("Please use 'window.display' instead")
+    def surface(self) -> DisplayBase:
+        return self._display
+    
+    @property
+    def display(self) -> DisplayBase:
+        return self._display
+    
     def clear(self, color = (0, 0, 0)):
         """
         Fill the entire surface with the given color
         Args:
             color (tuple[int, int, int], optional): RGB color to fill with. Defaults to (0, 0, 0).
         """
-        self.surface.fill(color)
+        self._display.fill(color)
 
     def _recalculate_render_area(self):
-        current_w, current_h = self.surface.get_size()
+        current_w, current_h = self._display.get_size()
         target_ratio = self._ratio or self._original_size
         self._crop_width_offset, self._crop_height_offset = self.cropToRatio(current_w, current_h, target_ratio)
         self._offset = NvVector2(self._crop_width_offset // 2, self._crop_height_offset // 2)
@@ -89,18 +125,18 @@ class Window:
             fps (int, optional): Desired frames per second. Defaults to 60.
         """
         self._next_update_dirty_rects.clear()
-        self.last_events = events
-        mouse.update(events)
-        time.update()
-        keyboard.update()
+        self.display.clear()
+        nevu_state.current_events = events
         
+        self._update_utils(events)
         for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
                 
-            if event.type == pygame.VIDEORESIZE and not self.resize_type == ResizeType.ResizeFromOriginal:
-                self.size = [event.w, event.h]
+            if event.type == pygame.VIDEORESIZE and self.resize_type != ResizeType.ResizeFromOriginal:
+                w, h = event.w, event.h
+                self.size = NvVector2(w, h)
                 self.z_system.mark_dirty()
                 if self.resize_type == ResizeType.CropToRatio:
                     self._recalculate_render_area()
@@ -112,9 +148,13 @@ class Window:
                 self._next_update_dirty_rects.append(pygame.Rect(0, 0, *self.size))
 
         self._clock.tick(fps)
-        self._event_cycle(EventType.Update)
         self.z_system.cycle(mouse.pos, mouse.left_fdown, mouse.left_up, mouse.any_wheel, mouse.wheel_down)
-
+        self._event_cycle(EventType.Update)
+        
+    def _update_utils(self, events):
+        mouse.update(events)
+        time.update()
+        keyboard.update()
     @property
     def offset(self):
         return self._offset
