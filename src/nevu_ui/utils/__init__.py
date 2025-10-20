6104479c7f012901433795fff628fd3b998eede7
GolemBebrov <bebrovgolem@gmail.com>
2025-10-20 16:45:48 +0300

    deleted Convert and Converted

--- src/nevu_ui/utils/__init__.py
+++ src/nevu_ui/utils/__init__.py
@@ -6,7 +6,6 @@ from .keyboard import Keyboard, keyboard
 from .convertor import Convertor
 from .event import NevuEvent, Event
 from .input_type import InputType
-
 __all__ = [
     'nvvector2_legacy', 'Cache', 'Mouse', 'mouse', 'Time', 'time', 'Keyboard', 'keyboard', 'Convertor', 'NevuEvent', 'Event', 'InputType'
 ]
