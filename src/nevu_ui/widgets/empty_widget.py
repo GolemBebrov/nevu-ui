7f750512842700a103bcb55f8a4f311eaa3288c7
GolemBebrov <bebrovgolem@gmail.com>
2025-10-25 13:07:16 +0300

    Simplified

--- src/nevu_ui/widgets/empty_widget.py
+++ src/nevu_ui/widgets/empty_widget.py
@@ -1,8 +1,16 @@
+from typing import Unpack
+
+from nevu_ui.fast.nvvector2.nvvector2 import NvVector2
+from nevu_ui.style import default_style
 from nevu_ui.widgets import Widget
-import copy
+from nevu_ui.widgets import WidgetKwargs
 
 class EmptyWidget(Widget):
+    def __init__(self, size: NvVector2 | list, **constant_kwargs: Unpack[WidgetKwargs]):
+        super().__init__(size, default_style, **constant_kwargs)
+        
     def draw(self):
         pass
+    
     def clone(self):
-        return EmptyWidget(self._lazy_kwargs['size'], copy.deepcopy(self.style), **self.constant_kwargs)
+        return EmptyWidget(self._lazy_kwargs['size'], **self.constant_kwargs)
