677f8d491806a19edcf0a218ae0ab82334c05fb3
GolemBebrov <bebrovgolem@gmail.com>
2025-10-26 19:31:11 +0300

    __init__ update

--- src/nevu_ui/rendering/__init__.py
+++ src/nevu_ui/rendering/__init__.py
@@ -5,5 +5,5 @@ from .triangle import Triangle
 from .rect import Rect, RoundedRect
 from .outlined_rect import OutlinedRect, OutlinedRoundedRect
 from .gradient import Gradient
-
-__all__ = ["FastBlit", "AlphaBlit", "Circle", "Line", "Triangle", "Rect", "RoundedRect", "OutlinedRect", "OutlinedRoundedRect", "Gradient"]
+from .shader import Shader
+__all__ = ["FastBlit", "AlphaBlit", "Circle", "Line", "Triangle", "Rect", "RoundedRect", "OutlinedRect", "OutlinedRoundedRect", "Gradient", "Shader"]
