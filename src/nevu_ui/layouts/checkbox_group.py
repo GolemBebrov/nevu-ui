7a423364f690371e92e8b861953c9e193dff3ec7
GolemBebrov <bebrovgolem@gmail.com>
2025-11-24 15:41:19 +0300

    Improved: Code Quality

--- src/nevu_ui/layouts/checkbox_group.py
+++ src/nevu_ui/layouts/checkbox_group.py
@@ -2,6 +2,18 @@ from nevu_ui.widgets import RectCheckBox
 from nevu_ui.core_types import EventType
 from nevu_ui.utils import NevuEvent
 
+#CHECKBOX_GROUP STRUCTURE: ====================
+#    Properties >
+#        all property functions
+#    Wrappers >
+#        wrappers for different modes
+#    Hooks >
+#        on_checkbox_added
+#        on_checkbox_toggled_multiple
+#        on_checkbox_toggled_single
+#    Functions >
+#        all other functions
+
 class CheckBoxGroup():
     def __init__(self, checkboxes: list[RectCheckBox] | None = None, single_select: bool = False):
         self._single_select = single_select
@@ -10,50 +22,55 @@ class CheckBoxGroup():
         if checkboxes is None: checkboxes = []
         for checkbox in checkboxes:
             self.add_checkbox(checkbox)
-    
+
+#=== Properties ===
+    @property
+    def current_checkboxes(self): return self._content
+
     @property
     def single_select(self): return self._single_select
-    
-    def on_checkbox_added(self, checkbox: RectCheckBox):
-        pass #hook
-    
-    def _on_checkbox_toggled_wrapper(self, checkbox: RectCheckBox):
+
+#=== Wrappers ===
+    def _on_toggle_multiple_wrapper(self, checkbox: RectCheckBox):
         toogled_checkboxes = []
         toogled_checkboxes.extend(checkbox for checkbox in self._content if checkbox.toogled)
-        self.on_checkbox_toggled(toogled_checkboxes)
+        self.on_checkbox_toggled_multiple(toogled_checkboxes)
     
-    def _on_checkbox_toggled_single_wrapper(self, checkbox: RectCheckBox):
+    def _on_toggle_single_wrapper(self, checkbox: RectCheckBox):
         if checkbox.toogled == False: return self.on_checkbox_toggled_single(None)
         for item in self._content:
             if item is not checkbox: item.toogled = False
         self.on_checkbox_toggled_single(checkbox)
-    
-    def on_checkbox_toggled(self, included_checkboxes: list[RectCheckBox]):
+        
+#=== Hooks ===
+    def on_checkbox_added(self, checkbox: RectCheckBox):
+    #=== hook ===
+        pass
+
+    def on_checkbox_toggled_multiple(self, included_checkboxes: list[RectCheckBox]):
     #=== hook ===
         pass
 
     def on_checkbox_toggled_single(self, checkbox: RectCheckBox | None):
     #=== hook ===
         pass 
-    
+
+#=== Methods ===
     def _add_copy(self, checkbox: RectCheckBox):
         self._content.append(checkbox)
         self.on_checkbox_added(checkbox)
     
     def add_checkbox(self, checkbox: RectCheckBox):
-        function = self._on_checkbox_toggled_single_wrapper if self.single_select else self._on_checkbox_toggled_wrapper
+        function = self._on_toggle_single_wrapper if self.single_select else self._on_toggle_multiple_wrapper
         checkbox.subscribe(NevuEvent(self, function, EventType.OnKeyDown))
         checkbox.subscribe(NevuEvent(self, self._add_copy, EventType.OnCopy))
         self._content.append(checkbox)
         self.on_checkbox_added(checkbox)
         
     def get_checkbox_by_id(self, id: str) -> RectCheckBox | None:
-        assert id, "id cant be None"
+        assert id, "Id can not be None."
         return next((item for item in self._content if item.id == id), None)
-    
-    @property
-    def current_checkboxes(self): return self._content
-    
+
     def add_event(self, event: NevuEvent):
         self._events.append(event)
 
