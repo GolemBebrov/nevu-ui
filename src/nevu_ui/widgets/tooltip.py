09c59d6c0e436bde8f22aee16803809b2af04293
GolemBebrov <bebrovgolem@gmail.com>
2025-11-26 02:37:38 +0300

    removed legacy classes and make deprecated

--- src/nevu_ui/widgets/tooltip.py
+++ src/nevu_ui/widgets/tooltip.py
@@ -5,8 +5,5 @@ class Tooltip():
         self.text = text
         self.style = style
         self.type = type.lower()
-        
         raise NotImplementedError("Tooltip is not implemented yet, wait till 0.6.X")
-    def draw(self):
-        pass #TODO in version 0.6 
-    
+    def draw(self): pass #TODO in version 0.6 
