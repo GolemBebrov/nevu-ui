e21a1287b0f045fc525ce63e0782065978a6bf21
GolemBebrov <bebrovgolem@gmail.com>
2025-11-25 15:32:40 +0300

    Huge refactor

--- src/nevu_ui/layouts/checkbox_group.py
+++ src/nevu_ui/layouts/checkbox_group.py
@@ -9,19 +9,16 @@ from nevu_ui.utils import NevuEvent
 #        wrappers for different modes
 #    Hooks >
 #        on_checkbox_added
-#        on_checkbox_toggled_multiple
-#        on_checkbox_toggled_single
+#        on_single_toggled
+#        on_multiple_toggled
 #    Functions >
 #        all other functions
 
 class CheckBoxGroup():
-    def __init__(self, checkboxes: list[RectCheckBox] | None = None, single_select: bool = False):
-        self._single_select = single_select
-        self._content: list[RectCheckBox] = []
-        self._events: list[NevuEvent] = []
-        if checkboxes is None: checkboxes = []
-        for checkbox in checkboxes:
-            self.add_checkbox(checkbox)
+    __slots__ = ["_content", "_events", "_single_select"]
+    def __init__(self, checkboxes: list[RectCheckBox] | None = None, single_selection: bool = False):
+        self._single_select = single_selection; self._content: list[RectCheckBox] = []; self._events: list[NevuEvent] = []
+        for checkbox in checkboxes or []: self.add_checkbox(checkbox)
 
 #=== Properties ===
     @property
@@ -31,50 +28,37 @@ class CheckBoxGroup():
     def single_select(self): return self._single_select
 
 #=== Wrappers ===
-    def _on_toggle_multiple_wrapper(self, checkbox: RectCheckBox):
-        toogled_checkboxes = []
-        toogled_checkboxes.extend(checkbox for checkbox in self._content if checkbox.toogled)
-        self.on_checkbox_toggled_multiple(toogled_checkboxes)
+    def _on_toggle_multiple_wrapper(self, checkbox: RectCheckBox): self.on_multiple_toggled([c for c in self._content if c.toogled])
     
     def _on_toggle_single_wrapper(self, checkbox: RectCheckBox):
-        if checkbox.toogled == False: return self.on_checkbox_toggled_single(None)
+        if checkbox.toogled == False: return self.on_single_toggled(None)
         for item in self._content:
             if item is not checkbox: item.toogled = False
-        self.on_checkbox_toggled_single(checkbox)
+        self.on_single_toggled(checkbox)
         
 #=== Hooks ===
-    def on_checkbox_added(self, checkbox: RectCheckBox):
-    #=== hook ===
-        pass
+    def on_checkbox_added(self, checkbox: RectCheckBox): pass #=== hook ===
 
-    def on_checkbox_toggled_multiple(self, included_checkboxes: list[RectCheckBox]):
-    #=== hook ===
-        pass
+    def on_multiple_toggled(self, included_checkboxes: list[RectCheckBox]): pass #=== hook ===
 
-    def on_checkbox_toggled_single(self, checkbox: RectCheckBox | None):
-    #=== hook ===
-        pass 
+    def on_single_toggled(self, checkbox: RectCheckBox | None): pass #=== hook ===
 
-#=== Methods ===
-    def _add_copy(self, checkbox: RectCheckBox):
+#=== Functions ===
+    def add_checkbox(self, checkbox: RectCheckBox):
+        checkbox.subscribe(NevuEvent(self, self._on_toggle_single_wrapper if self.single_select else self._on_toggle_multiple_wrapper, EventType.OnKeyDown))
+        checkbox.subscribe(NevuEvent(self, self._sub_add, EventType.OnCopy))
+        self._sub_add(checkbox)
+    
+    def _sub_add(self, checkbox: RectCheckBox):
         self._content.append(checkbox)
         self.on_checkbox_added(checkbox)
     
-    def add_checkbox(self, checkbox: RectCheckBox):
-        function = self._on_toggle_single_wrapper if self.single_select else self._on_toggle_multiple_wrapper
-        checkbox.subscribe(NevuEvent(self, function, EventType.OnKeyDown))
-        checkbox.subscribe(NevuEvent(self, self._add_copy, EventType.OnCopy))
-        self._content.append(checkbox)
-        self.on_checkbox_added(checkbox)
-        
     def get_checkbox_by_id(self, id: str) -> RectCheckBox | None:
         assert id, "Id can not be None."
         return next((item for item in self._content if item.id == id), None)
+    
+    def add_event(self, event: NevuEvent): self._events.append(event)
 
-    def add_event(self, event: NevuEvent):
-        self._events.append(event)
-
-    def _event_cycle(self, type: EventType, *args, **kwargs):
+    def _event_cycle(self, event_type: EventType, *args, **kwargs):
         for event in self._events:
-            if event._type == type:
-                event(*args, **kwargs)
+            if event._type == event_type: event(*args, **kwargs)
