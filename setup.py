03a5198467bede5f7c33a2491b450060ecf92a25
GolemBebrov <bebrovgolem@gmail.com>
2025-12-21 00:59:56 +0300

    improved setup

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, Extension
 import numpy
-def get_numpy_include():
-    import numpy
-    return numpy.get_include()
+import os
+
+c_opts = ['-O3', '-march=native', '-ffast-math', '-fopenmp']
+l_opts = ['-fopenmp']
+
+cython_directives = {
+    'language_level': "3",
+    'boundscheck': False,  
+    'wraparound': False,  
+    'cdivision': True,   
+    'nonecheck': False,   
+    'initializedcheck': False 
+}
 
 def get_extensions():
     from Cython.Build import cythonize
@@ -12,29 +22,31 @@ def get_extensions():
             "nevu_ui.fast.nvvector2.nvvector2",
             ["src/nevu_ui/fast/nvvector2/nvvector2.pyx"],
             include_dirs=[numpy.get_include()],
+            extra_compile_args=['-O3', '-march=native', '-ffast-math'],
         ),
         Extension(
             "nevu_ui.fast.logic.fast_logic",
             ["src/nevu_ui/fast/logic/fast_logic.pyx"],
-            include_dirs=[get_numpy_include()]
+            extra_compile_args=c_opts, 
+            extra_link_args=l_opts,
+            include_dirs=[numpy.get_include()]
         ),
-
         Extension(
             "nevu_ui.fast.shapes.fast_shapes",
             ["src/nevu_ui/fast/shapes/fast_shapes.pyx"],
-            extra_compile_args=['-fopenmp'], 
-            extra_link_args=['-fopenmp'],
-            include_dirs=[get_numpy_include()]
+            extra_compile_args=c_opts, 
+            extra_link_args=l_opts,
+            include_dirs=[numpy.get_include()]
         ),
         Extension(
             "nevu_ui.fast.zsystem.fast_zsystem",
             ["src/nevu_ui/fast/zsystem/fast_zsystem.pyx"],
             include_dirs=[numpy.get_include()],
+            extra_compile_args=['-O3'],
         )
-
     ]
-    return cythonize(extensions, compiler_directives={'language_level': "3"})
-
+    
+    return cythonize(extensions, compiler_directives=cython_directives)
 
 setup(
     ext_modules = get_extensions()
