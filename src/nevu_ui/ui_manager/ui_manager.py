6f9c3ebca47c55b1570434649d02da753124614f
GolemBebrov <bebrovgolem@gmail.com>
2025-12-21 00:38:13 +0300

    Critical Change of API, removed dirty_rect support

--- src/nevu_ui/ui_manager/ui_manager.py
+++ src/nevu_ui/ui_manager/ui_manager.py
@@ -7,65 +7,55 @@ class Manager:
     def __init__(self, window: Window | None = None):
         if window: self.window = window
         self.running = True
-        self._dirty_mode = False
-        self._force_quit = True
+        self.dirty_mode = False
+        self.force_quit = True
         self.background = (0, 0, 0)
         self.fps = 60
 
     @property
-    def window(self):
-        return self._window
+    def window(self): return self._window
     @window.setter
     def window(self, window: Window):
         if not isinstance(window, Window):
             raise ValueError("Unexpected window type!")
         self._window = window
 
-    def _before_draw_loop(self):
+    def _before_draw(self):
         self.window.clear(self.background)
-    def draw_loop(self):
-        pass
-    def _after_draw_loop(self):
-        pass
-
+    def on_draw(self): pass
+    def _after_draw(self): pass
+    
     def __main_draw_loop(self):
-        self._before_draw_loop()
-        self.draw_loop()
-        self._after_draw_loop()
+        self._before_draw()
+        self.on_draw()
+        self._after_draw()
 
-    def _before_update_loop(self, events):
-        pass
-    def update_loop(self, events):
-        pass
-    def _after_update_loop(self, events):
+    def _before_update(self, events): pass
+    def on_update(self, events): pass
+    def _after_update(self, events):
         self.window.update(events, self.fps)
 
     def __main_update_loop(self):
         events = pygame.event.get()
-        self._before_update_loop(events)
-        self.update_loop(events)
-        self._after_update_loop(events)
+        self._before_update(events)
+        self.on_update(events)
+        self._after_update(events)
     
     def exit(self):
         self.running = False
     
-    def on_start(self):
-        pass
+    def on_start(self): pass
     
-    def on_exit(self):
-        pass 
+    def on_exit(self): pass 
     
     def _on_exit(self):
         self.on_exit()
-        if self._force_quit:
+        if self.force_quit:
             pygame.quit()
             sys.exit()
         
-    def first_update(self):
-        pass
-    
-    def first_draw(self):
-        pass
+    def first_update(self): pass
+    def first_draw(self): pass
     
     def __main_loop(self):
         self.on_start()
@@ -74,13 +64,12 @@ class Manager:
         while self.running:
             self.__main_update_loop()
             self.__main_draw_loop()
-            if self._dirty_mode:
+            if self.dirty_mode:
                 self.window.display.update() 
-                #pygame.display.update(self.window._next_update_dirty_rects)
+                #pygame.display.update(self.window._next_update_dirty_rects) #TODO: Dirty rects will be reimplemented
             else:
                 self.window.display.update()
             self.window._next_update_dirty_rects = []
         self._on_exit()
         
-    def run(self):
-        self.__main_loop()
+    def run(self): self.__main_loop()
