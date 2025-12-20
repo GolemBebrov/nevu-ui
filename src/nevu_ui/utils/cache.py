efa5c49c08ac3a7e8fd05e37d1a1176608e7de0a
GolemBebrov <bebrovgolem@gmail.com>
2025-12-21 00:39:01 +0300

    Make code smaller and easier to extend

--- src/nevu_ui/utils/cache.py
+++ src/nevu_ui/utils/cache.py
@@ -1,83 +1,65 @@
-from nevu_ui.core_types import (
+from nevu_ui.core.enums import (
     CacheName, CacheType
 )
 
 class Cache:
-    __slots__ = ("name", "cache", "cache_default")
+    __slots__ = ("name", "namespaces", "cache_default")
     def __init__(self):
         self.name = CacheName.MAIN
-        self.cache_default = {
-            CacheType.Coords: None,
-            CacheType.RelSize: None,
-            CacheType.Surface: None,
-            CacheType.Gradient: None,
-            CacheType.Image: None,
-            CacheType.Borders: None,
-            CacheType.Scaled_Borders: None,
-            CacheType.Scaled_Background: None,
-            CacheType.Background: None,
-            CacheType.Scaled_Gradient: None,
-            CacheType.Scaled_Image: None,
-            CacheType.Texture: None
-            
-        }
-        self.cache = {
+        self.cache_default = {member: None for member in CacheType}
+        self.namespaces = {
             CacheName.MAIN: self.cache_default.copy(),
-            CacheName.PREVERSED: self.cache_default.copy(),
-            CacheName.CUSTOM: self.cache_default.copy()
-        }
+            CacheName.PRESERVED: self.cache_default.copy(),
+            CacheName.CUSTOM: self.cache_default.copy()}
         
-    def set_name(self, name: CacheName):
-        self.name = name
-        
+    def set_name(self, name: CacheName): self.name = name
+    
     def clear(self, name = None):
         name = name or self.name
-        self.cache[name] = self.cache_default.copy()
+        self.namespaces[name] = self.cache_default.copy()
         
     def clear_selected(self, blacklist = None, whitelist = None, name = None):
         name = name or self.name
-        cachename = self.cache[name]
-        blacklist = [] if blacklist is None else blacklist
-        whitelist = [CacheType.RelSize,
-                     CacheType.Coords,
-                     CacheType.Surface,
-                     CacheType.Gradient,
-                     CacheType.Image,
-                     CacheType.Scaled_Image,
-                     CacheType.Borders,
-                     CacheType.Scaled_Borders,
-                     CacheType.Scaled_Background,
-                     CacheType.Scaled_Gradient,
-                     CacheType.Background,
-                     CacheType.Texture,
-                    ] if whitelist is None else whitelist
+        cachename = self.namespaces[name]
+        blacklist = blacklist or []
+        whitelist = whitelist or CacheType
         for item, value in cachename.items():
             if item not in blacklist and item in whitelist:
                 cachename[item] = None
                 
     def get(self, type: CacheType, name = None):
         name = name or self.name
-        return self.cache[name][type]
+        return self.namespaces[name][type]
     
     def set(self, type: CacheType, value, name = None):
         name = name or self.name
-        self.cache[name][type] = value
-        
+        self.namespaces[name][type] = value
+    
+    @property
+    def current_namespace(self): return self.namespaces[self.name]
+    
     def get_or_set_val(self, type: CacheType, value, name = None):
         name = name or self.name
-        if self.cache[name][type] is None:
-            self.cache[name][type] = value
-        return self.cache[name][type]
+        if self.namespaces[name][type] is None:
+            self.namespaces[name][type] = value
+        return self.namespaces[name][type]
     
     def get_or_exec(self, type: CacheType, func, name = None):
         name = name or self.name
-        if self.cache[name][type] is None:
-            self.cache[name][type] = func()
-        return self.cache[name][type]
+        if self.namespaces[name][type] is None:
+            self.namespaces[name][type] = func()
+        return self.namespaces[name][type]
     
-    def __getattr__(self, type):
-        return self.cache[self.name][type]
+    def __getattr__(self, type): return self.namespaces[self.name][type]
     def __getitem__(self, key: CacheType):
-        if not isinstance(key, CacheType):
-            raise TypeError("Ключ для доступа к кешу должен быть типа CacheType")
-        return self.cache[self.name][key]
+        if not isinstance(key, CacheType): raise TypeError("The key for cache access must be of type CacheType")
+        return self.namespaces[self.name][key]
+
+    def copy(self):
+        copy = Cache()
+        copy.name = self.name
+        copy.namespaces = self.namespaces.copy()
+        return copy
+    
+    def __deepcopy__(self, memo): 
+        return self.copy()
