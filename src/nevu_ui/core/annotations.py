ec0be7dad717fe8f65b68da498a109f230d307cb
GolemBebrov <bebrovgolem@gmail.com>
2026-04-18 22:05:55 +0300

    Added: nevuobj_size/style

--- src/nevu_ui/core/annotations.py
+++ src/nevu_ui/core/annotations.py
@@ -1,3 +1,11 @@
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from nevu_ui.presentation.style import Style
+from nevu_ui.core.size.units import SizeRule
+from nevu_ui.fast.nvvector2.nvvector2 import NvVector2
+
+
 class Annotations:
     #=== size annotations ===
     any_number = float | int
@@ -9,5 +17,11 @@ class Annotations:
     rgba_color = tuple[int, int, int, int]
     rgb_like_color = rgb_color | rgba_color
     hsl_color = tuple[float, float, float]
+    hsla_color = tuple[float, float, float, int]
     hex_color = str
     any_color = rgb_like_color | hsl_color | hex_color
+    
+    #=== NevuObject annotation ===
+    size_item = int | SizeRule | float
+    nevuobj_size = tuple[size_item, size_item] | list[size_item] | NvVector2 | None
+    nevuobj_style = Any | str | None
