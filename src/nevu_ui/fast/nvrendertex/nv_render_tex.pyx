fed4c395d7887c1e12ed52756a977c66b29c357d
GolemBebrov <bebrovgolem@gmail.com>
2026-04-20 14:47:47 +0300

    Highly optimized NvRenderTexture

--- src/nevu_ui/fast/nvrendertex/nv_render_tex.pyx
+++ src/nevu_ui/fast/nvrendertex/nv_render_tex.pyx
@@ -3,7 +3,8 @@ from nevu_ui.fast.nvvector2.nvvector2 cimport NvVector2
 from nevu_ui.fast.nvrect.nvrect cimport NvRect
 from nevu_ui.presentation.color import Color
 from nevu_ui.core.state import nevu_state
-from nevu_ui.core.annotations import Annotations        
+from nevu_ui.core.annotations import Annotations      
+from nevu_ui.fast.raylib.nevu_raylib cimport begin_blend_mode, end_blend_mode, begin_texture_mode, end_texture_mode, c_draw_texture_rec, c_draw_texture_vec, clear_background, c_clear_background
 
 cdef class NvRenderTexture:
     cdef public object render_texture 
@@ -31,29 +32,57 @@ cdef class NvRenderTexture:
     @property
     def depth(self): return self.render_texture.depth
     
-    cpdef void blit(self, nv_texture: NvRenderTexture, dest: Annotations.dest_like, blend_mode: int = 0, flip: bool = True, color: Annotations.rgba_color = Color.White):
+    cpdef void blit(self, NvRenderTexture nv_texture, dest: Annotations.dest_like, int blend_mode = 0, bint flip = True, tuple color = Color.White):
         assert self.loaded, "Render texture not loaded"
-        md.rl.begin_texture_mode(self.render_texture) # type: ignore
-        self.fast_blit(nv_texture, dest, flip, color)
-        md.rl.end_texture_mode()
+        begin_blend_mode(blend_mode)
+        begin_texture_mode(self.render_texture) # type: ignore
+        self.c_fast_blit(nv_texture, dest, flip, color)
+        end_texture_mode()
+        end_blend_mode()
     
-    cpdef void fast_blit(self, nv_texture: NvRenderTexture, dest: Annotations.dest_like, flip: bool = True, color: Annotations.rgba_color = Color.White):
+    cdef inline void c_fast_blit(self, NvRenderTexture nv_texture, tuple dest, bint flip = True, tuple color = Color.White):
+        h = -nv_texture.height if flip else nv_texture.height
+        cdef Py_ssize_t dest_len = <Py_ssize_t>len(dest)
+        if dest_len == 2:
+            c_draw_texture_vec(nv_texture.render_texture.texture, dest, color, flip)
+        elif dest_len == 4:
+            c_draw_texture_rec(nv_texture.render_texture.texture, (0,0, nv_texture.width, h), dest, color)
+        else:
+            raise ValueError(f"Invalid dest argument - {dest}, must be (x, y) or (x, y, width, height)")
+
+    cpdef void fast_blit(self, NvRenderTexture nv_texture, tuple dest, bint flip = True, tuple color = Color.White):
+        self.c_fast_blit(nv_texture, dest, flip, color)
+    
+    cpdef void blit_texture(self, texture: object, dest: Annotations.dest_like, int blend_mode = 0, bint flip = True, tuple color = Color.White):
+        assert self.loaded, "Render texture not loaded"
+        begin_texture_mode(self.render_texture) # type: ignore
+        self.fast_blit_texture(texture, dest, flip, color)
+        end_texture_mode()
+    
+    cpdef void fast_blit_texture(self, texture: object, dest: Annotations.dest_like, bint flip = True, tuple color = Color.White):
         display = nevu_state.window.display
         assert nevu_state.window.is_raylib(display)
-        display.fast_blit_pro(nv_texture.texture, dest, flip, color)
-        
-    cpdef void clear(self, color: Annotations.rgba_color):
+        display.fast_blit_pro(texture, dest, flip, color)
+
+    cdef inline void c_fast_clear(self, tuple color):
+        c_clear_background(color)
+    
+    cdef inline void c_clear(self, tuple color):
+        begin_texture_mode(self.render_texture) # type: ignore
+        self.c_fast_clear(color)
+        end_texture_mode()
+
+    cpdef void clear(self, tuple color):
         assert self.loaded, "Render texture not loaded"
-        md.rl.begin_texture_mode(self.render_texture) # type: ignore
-        self.fast_clear(color)
-        md.rl.end_texture_mode()
+        self.c_clear(color)
     
-    cpdef void fast_clear(self, color: Annotations.rgba_color):
-        display = nevu_state.window.display
-        assert nevu_state.window.is_raylib(display)
-        display.clear(color)
+    cpdef void fast_clear(self, tuple color):
+        assert self.loaded, "Render texture not loaded"
+        self.c_fast_clear(color)
     
-    cpdef void fill(self, color: Annotations.rgba_color): self.clear(color)
+    cpdef void fill(self, tuple color): 
+        assert self.loaded, "Render texture not loaded"
+        self.c_clear(color)
         
     cpdef void kill(self):
         assert self.loaded, "Render texture not loaded"
@@ -85,8 +114,8 @@ cdef class NvRenderTexture:
         
     def __enter__(self):
         assert self.loaded, "Render texture not loaded"
-        md.rl.begin_texture_mode(self.render_texture) # type: ignore
+        begin_texture_mode(self.render_texture) # type: ignore
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        md.rl.end_texture_mode()
+        end_texture_mode()
