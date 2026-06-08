22c7a2b439dab79c7f6be2095bb1efb000a89896
GolemBebrov <bebrovgolem@gmail.com>
2026-06-08 12:48:18 +0300

    MAJOR optimization, mostly for GC

--- src/nevu_ui/fast/zsystem/fast_zsystem.pxd
+++ src/nevu_ui/fast/zsystem/fast_zsystem.pxd
@@ -1,7 +1,12 @@
 cimport numpy as np
 from nevu_ui.fast.nvvector2.nvvector2 cimport NvVector2
 from nevu_ui.fast.nevucobj.nevucobj cimport NevuCobject
+
 cdef class ZRequest:
+    @staticmethod
+    cdef ZRequest new(link, 
+                  on_hover_func, on_unhover_func, on_click_func, 
+                  on_keyup_func, on_keyup_abandon_func, on_scroll_func)
     cdef object _link_ref
     cdef public object on_hover_func
     cdef public object on_unhover_func
@@ -14,13 +19,15 @@ cdef class ZRequest:
 
 cdef class ZSystem:
     cdef list _registered_requests
-    cdef object last_hovered_request
-    cdef object clicked_request  
-    cdef list live_requests
-    cdef bint is_dirty
+    cdef public object last_hovered_request
+    cdef public object clicked_request  
+    cdef public list live_requests
+    cdef public bint is_dirty
 
     cdef np.ndarray rects_data
     cdef np.ndarray z_indices
+    cdef int[:, ::1] rects_view
+    cdef int[::1] z_view
 
     cdef void c_mark_dirty(self) nogil
     cdef void _reset_arrays(self)
@@ -29,4 +36,4 @@ cdef class ZSystem:
     cpdef mark_dirty(self)
     cpdef list get_active_requests(self)
     cpdef cycle(self, NvVector2 mouse_pos, bint mouse_down, bint mouse_up, bint any_wheel, bint wheel_down)
-    cdef object request_cycle(self, NvVector2 mouse_pos, bint any_wheel, bint wheel_down, list live_requests)
+    cdef ZRequest request_cycle(self, NvVector2 mouse_pos, bint any_wheel, bint wheel_down)
--- src/nevu_ui/fast/zsystem/fast_zsystem.pyx
+++ src/nevu_ui/fast/zsystem/fast_zsystem.pyx
@@ -13,9 +13,24 @@ import weakref
 from nevu_ui.fast.nvvector2.nvvector2 cimport NvVector2
 from nevu_ui.fast.nvrect.nvrect cimport NvRect
 from nevu_ui.fast.nevucobj.nevucobj cimport NevuCobject
+
 np.import_array()
 
 cdef class ZRequest:
+    @staticmethod
+    cdef ZRequest new(link, 
+                  on_hover_func, on_unhover_func, on_click_func, 
+                  on_keyup_func, on_keyup_abandon_func, on_scroll_func):
+        cdef ZRequest req = ZRequest.__new__(ZRequest)
+        req._link_ref = weakref.ref(link)
+        req.on_hover_func = on_hover_func
+        req.on_unhover_func = on_unhover_func
+        req.on_click_func = on_click_func
+        req.on_scroll_func = on_scroll_func
+        req.on_keyup_func = on_keyup_func
+        req.on_keyup_abandon_func = on_keyup_abandon_func
+        return req
+        
     def __init__(self, link,
                  on_hover_func=None, on_unhover_func=None, on_click_func=None, 
                  on_keyup_func=None, on_keyup_abandon_func=None, on_scroll_func=None):
@@ -42,15 +57,15 @@ cdef class ZSystem:
     cdef void _reset_arrays(self):
         self.rects_data = np.empty((0, 4), dtype=np.int32)
         self.z_indices = np.empty(0, dtype=np.int32)
+        self.rects_view = self.rects_data
+        self.z_view = self.z_indices
 
     cpdef void add(self, ZRequest z_request):
         self._registered_requests.append(weakref.ref(z_request))
-        self.mark_dirty()
+        self.is_dirty = True
         
     cdef void _rebuild_arrays(self):
         cdef list live_requests_refs = []
-        cdef list rect_list = []
-        cdef list z_list = []
         cdef list temp_live_requests = []
         
         cdef object req_ref
@@ -58,28 +73,44 @@ cdef class ZSystem:
         cdef NvRect current_rect
         cdef NevuCobject current_link
         
+        cdef Py_ssize_t max_len = len(self._registered_requests)
+        cdef np.ndarray[np.int32_t, ndim=2] new_rects = np.empty((max_len, 4), dtype=np.int32)
+        cdef np.ndarray[np.int32_t, ndim=1] new_zs = np.empty(max_len, dtype=np.int32)
+        
+        cdef int[:, ::1] r_view = new_rects
+        cdef int[::1] z_view = new_zs
+        
+        cdef Py_ssize_t valid_count = 0
+        
         for req_ref in self._registered_requests:
             req = req_ref()
             
             if req is not None:
                 current_link = req.get_link()
-                if current_link is not None:
-                    if current_link._dead: continue
+                if current_link is not None and not current_link._dead:
                     live_requests_refs.append(req_ref)
-                    current_rect = current_link.get_nvrect()
-                    rect_list.append((current_rect.x, current_rect.y, current_rect.w, current_rect.h))
-                    z_list.append(current_link.get_param_strict("z").value)
                     temp_live_requests.append(req)
+                    
+                    current_rect = current_link.get_nvrect()
+                    r_view[valid_count, 0] = <int>current_rect.x
+                    r_view[valid_count, 1] = <int>current_rect.y
+                    r_view[valid_count, 2] = <int>current_rect.w
+                    r_view[valid_count, 3] = <int>current_rect.h
+                    z_view[valid_count] = current_link.get_param_strict("z").value
+                    
+                    valid_count += 1
                     
         self._registered_requests = live_requests_refs
         self.live_requests = temp_live_requests
         
-        if not temp_live_requests:
+        if valid_count == 0:
             self._reset_arrays()
             return
 
-        self.rects_data = np.array(rect_list, dtype=np.int32)
-        self.z_indices = np.array(z_list, dtype=np.int32)
+        self.rects_data = new_rects[:valid_count]
+        self.z_indices = new_zs[:valid_count]
+        self.rects_view = self.rects_data
+        self.z_view = self.z_indices
     
     cpdef mark_dirty(self):
         self.is_dirty = True
@@ -98,82 +129,82 @@ cdef class ZSystem:
             self._rebuild_arrays()
             self.is_dirty = False
 
-        cdef object current_winner_request = self.request_cycle(mouse_pos, any_wheel, wheel_down, self.live_requests)
-        cdef ZRequest clicked_request, winner_request_z, hovered_request_z
-
+        cdef ZRequest current_winner_request = self.request_cycle(mouse_pos, any_wheel, wheel_down)
+        cdef ZRequest clicked_req
+        
         if self.clicked_request is None:
             if self.last_hovered_request is not current_winner_request:
-                
                 if self.last_hovered_request is not None:
-                    hovered_request_z = self.last_hovered_request
-                    if hovered_request_z.on_unhover_func is not None:
-                        hovered_request_z.on_unhover_func()
+                    if (<ZRequest>self.last_hovered_request).on_unhover_func is not None:
+                        (<ZRequest>self.last_hovered_request).on_unhover_func()
                 
                 if current_winner_request is not None:
-                    winner_request_z = current_winner_request
-                    if winner_request_z.on_hover_func is not None:
-                        winner_request_z.on_hover_func()
+                    if current_winner_request.on_hover_func is not None:
+                        current_winner_request.on_hover_func()
 
             self.last_hovered_request = current_winner_request
         
         if mouse_down and current_winner_request is not None:
-            winner_request_z = current_winner_request
-            clicked_request = winner_request_z
-            self.clicked_request = clicked_request
-            if clicked_request.on_click_func is not None:
-                clicked_request.on_click_func()
+            self.clicked_request = current_winner_request
+            if current_winner_request.on_click_func is not None:
+                current_winner_request.on_click_func()
         
         if mouse_up:
             if self.clicked_request is not None:
-                clicked_request = self.clicked_request
-                if clicked_request.get_link() is not None:
-                    if clicked_request is current_winner_request:
-                        if clicked_request.on_keyup_func is not None:
-                            clicked_request.on_keyup_func()
+                clicked_req = <ZRequest>self.clicked_request
+                if clicked_req.get_link() is not None:
+                    if clicked_req is current_winner_request:
+                        if clicked_req.on_keyup_func is not None:
+                            clicked_req.on_keyup_func()
                     else:
-                        if clicked_request.on_keyup_abandon_func is not None:
-                            clicked_request.on_keyup_abandon_func()
+                        if clicked_req.on_keyup_abandon_func is not None:
+                            clicked_req.on_keyup_abandon_func()
                 self.clicked_request = None
 
-    cdef object request_cycle(self, NvVector2 mouse_pos, bint any_wheel, bint wheel_down, list live_requests):
-        if not live_requests:
+    cdef ZRequest request_cycle(self, NvVector2 mouse_pos, bint any_wheel, bint wheel_down):
+        if not self.live_requests:
             return None
-        
-        cdef np.ndarray collided_mask
-        cdef np.ndarray[np.intp_t, ndim=1] candidate_indices
-        cdef np.ndarray[np.intp_t, ndim=1] sorted_indices
-        cdef ZRequest candidate_req
-        cdef NevuCobject candidate_link
-        
+            
         cdef int mx = int(mouse_pos.x)
         cdef int my = int(mouse_pos.y)
         
-        cdef np.ndarray[np.int32_t, ndim=2] rects = self.rects_data
+        cdef Py_ssize_t i
+        cdef Py_ssize_t best_idx = -1
+        cdef int best_z = -2147483648
+        cdef int current_z
+        cdef ZRequest candidate_req
+        cdef NevuCobject candidate_link
+        
+        cdef Py_ssize_t num_rects = self.rects_view.shape[0]
 
-        if len(live_requests) != rects.shape[0]:
+        if len(self.live_requests) != num_rects:
             return None
         
-        collided_mask = ((rects[:, 0] <= mx) & 
-                         (mx < rects[:, 0] + rects[:, 2]) &
-                         (rects[:, 1] <= my) & 
-                         (my < rects[:, 1] + rects[:, 3]))
-
-        candidate_indices = np.where(collided_mask)[0]
-        
-        if candidate_indices.size > 0:
-            sorted_indices = candidate_indices[np.argsort(self.z_indices[candidate_indices])[::-1]]
-            
-            for idx in sorted_indices:
-                candidate_req = live_requests[idx]
-                candidate_link = candidate_req.get_link()
+        for i in range(num_rects):
+            if (mx >= self.rects_view[i, 0] and 
+                mx < self.rects_view[i, 0] + self.rects_view[i, 2] and
+                my >= self.rects_view[i, 1] and 
+                my < self.rects_view[i, 1] + self.rects_view[i, 3]):
                 
-                if candidate_link is None:
-                    self.mark_dirty()
-                    continue
+                current_z = self.z_view[i]
+                
+                if current_z > best_z:
+                    candidate_req = <ZRequest>self.live_requests[i]
+                    candidate_link = candidate_req.get_link()
+                    
+                    if candidate_link is None:
+                        self.is_dirty = True
+                        continue
+                    
+                    best_z = current_z
+                    best_idx = i
+                    
+        if best_idx != -1:
+            candidate_req = <ZRequest>self.live_requests[best_idx]
             
-                if any_wheel and candidate_req.on_scroll_func is not None:
-                    candidate_req.on_scroll_func(wheel_down)
-        
-                return candidate_req
+            if any_wheel and candidate_req.on_scroll_func is not None:
+                candidate_req.on_scroll_func(wheel_down)
+                
+            return candidate_req
             
         return None
