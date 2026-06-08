7d695f0833e55a047c98c71ef26fa366517e28b6
GolemBebrov <bebrovgolem@gmail.com>
2026-06-08 12:35:34 +0300

    NevuCobject V2: New architecture + Optimizations

--- src/nevu_ui/fast/nevucobj/nevucobj.pxd
+++ src/nevu_ui/fast/nevucobj/nevucobj.pxd
@@ -11,15 +11,18 @@ from libcpp.vector cimport vector
 from nevu_ui.fast.nvparam.nvparam cimport NvParam
 from nevu_ui.fast.nvrect.nvrect cimport NvRect
 from nevu_ui.fast.nevucache.nevucache cimport Cache
-
+from nevu_ui.fast.zsystem.fast_zsystem cimport ZRequest
 cdef class NevuCobject:
     cdef public bint _sended_z_link, _dragging, _is_kup, _kup_abandoned, _force_state_set_continue, _visible, _active, _changed, _first_update, _wait_mode, _dead, booted
+    cdef public bint _custom_secondary_update, _custom_animation_update, _custom_logic_update, _custom_event_update
     cdef public NvVector2 coordinates, absolute_coordinates, size, _resize_ratio
-    cdef public object _style, animation_manager
-    cdef public list[NvParam] params
-    cdef public dict _params_map 
+    cdef public object _style, animation_manager, hover_state
+    cdef public list params, _blacklisted_params, _next_frame_functions
+    cdef public dict _params_map, _param_links
     cdef public Cache cache
+    cdef ZRequest _z_request
     cdef list specific_cache_whitelist
+
     cpdef list _get_param_names(self)
     cpdef object _find_param(self, str name)
     cpdef void _add_param(self, str name, supported_classes, default, getter, setter, int layer)
@@ -37,4 +40,40 @@ cdef class NevuCobject:
     cpdef NvRect get_nvrect(self)
     cpdef set_coordinates(self, NvVector2 coordinates)
     cpdef void clear_all(self)
+    cdef inline void c_set_coords_xy(self, double x, double y) noexcept
     cpdef void clear_surfaces(self)
+    cdef inline NvVector2 c_get_actual_size(self)
+    cpdef update(self)
+    cdef inline void _primary_update(self, events)
+    cdef inline void _base_animation_update(self)
+    cdef inline void _base_logic_update(self)
+
+
+    cpdef _on_click_system(self)
+    cpdef _on_hover_system(self)
+    cpdef _on_keyup_system(self)
+    cpdef _on_keyup_abandon_system(self)
+    cpdef _on_unhover_system(self)
+    cpdef _on_scroll_system(self, bint side)
+    cpdef _on_change_system(self)
+
+    cpdef _group_on_click(self)
+    cpdef _group_on_hover(self)
+    cpdef _group_on_keyup(self)
+    cpdef _group_on_keyup_abandon(self)
+    cpdef _group_on_unhover(self)
+    cpdef _group_on_scroll(self, bint side)
+
+    cpdef _click(self)
+    cpdef draw(self)
+    cpdef _unhover(self)
+    cpdef _hover(self)
+    cpdef _kup(self)
+    #cpdef _scroll(self, bint scrolled)
+    cpdef _kup_abandon(self)
+    cdef inline void _core_event_cycle(self, object type, list content, tuple args, dict kwargs)
+    cdef inline void _core_event_cycle_clear(self, object type)
+   # cpdef draw(self)
+    cdef inline void _base_secondary_draw_end(self)
+    cdef inline void _base_secondary_draw(self)
+    cdef set_hover_state(self, value)
--- src/nevu_ui/fast/nevucobj/nevucobj.pyx
+++ src/nevu_ui/fast/nevucobj/nevucobj.pyx
@@ -10,12 +10,22 @@ from nevu_ui.fast.nvvector2.nvvector2 cimport NvVector2
 from nevu_ui.fast.nvparam.nvparam cimport NvParam
 from cpython.dict cimport PyDict_GetItem
 from cpython.object cimport PyObject
+from cpython.list cimport PyList_GET_SIZE, PyList_GET_ITEM
 from nevu_ui.fast.nevucache.nevucache cimport Cache
 from nevu_ui.core.state import nevu_state
 from nevu_ui.core.enums import CacheType
+from nevu_ui.core.classes import _strategy_type, Strategy
+from nevu_ui.fast.zsystem.fast_zsystem cimport ZSystem, ZRequest
+from nevu_ui.core.enums import (
+    HoverState, EventType, CacheType, ConstantLayer, AnimationType
+)
+
+
 cimport cython
 cdef extern from "Python.h":
     object PyObject_CallNoArgs(object func)
+
+call_noarg = PyObject_CallNoArgs
 
 from nevu_ui.fast.logic.fast_logic cimport relm_helper, rel_helper, mass_rel_helper, vec_rel_helper, get_nvrect_helper
 from nevu_ui.fast.nvrect.nvrect cimport NvRect
@@ -39,6 +49,14 @@ cdef class NevuCobject:
         self._force_state_set_continue = False
         self._visible = True
         self._active = True
+        self._custom_secondary_update = False
+        self._custom_animation_update = False
+        self._custom_logic_update = False
+        self._custom_event_update = False
+        self._custom_primary_draw = False
+        self._custom_secondary_draw = False
+        self._custom_secondary_draw_content = False
+        self._custom_secondary_draw_end = False
         self._changed = True
         self._first_update = True
         self.booted = False
@@ -74,7 +92,7 @@ cdef class NevuCobject:
         if p_ptr == NULL: return None 
         cdef NvParam param = <NvParam>p_ptr
         if param.getter is not None:
-            return PyObject_CallNoArgs(param.getter)
+            return call_noarg(param.getter)
         return param.value
 
     cpdef void set_param_value(self, str name, object new_value):
@@ -85,6 +103,7 @@ cdef class NevuCobject:
         if param.setter is not None:
             param.setter(new_value)
         else: param.value = new_value
+        
 
     cpdef double relx_custom(self, double num, double min, double max):
         return rel_helper(num, self._resize_ratio.x, min, max)
@@ -117,12 +136,20 @@ cdef class NevuCobject:
         if not need_to_set: return
         self.coordinates = coordinates
     
+    cdef inline void c_set_coords_xy(self, double x, double y) noexcept:
+        if self.coordinates.x == x and self.coordinates.y == y:
+            return
+        cdef NvVector2 new_coords = NvVector2.new(x, y)
+        cdef bint need_to_set = self._coordinates_setter(new_coords) #type: ignore
+        if not need_to_set: return
+        self.coordinates = new_coords
+    
     cpdef void clear_all(self):
         """
         Clears all cached data by invoking the clear method on the cache. 
         !WARNING!: may cause bugs and errors
         """
-        if nevu_state.window.is_dtype.raylib: 
+        if nevu_state.window.renderer_type.raylib: 
             self._clear_rl_specific()
         self.cache.c_clear()
         
@@ -133,6 +160,217 @@ cdef class NevuCobject:
         This includes Image, Scaled_Gradient, Surface, and Borders.
         Highly recommended to use this method instead of clear_all.
         """
-        if nevu_state.window.is_dtype.raylib: 
-            self._clear_rl_specific()
+        if nevu_state.window.renderer_type.raylib: 
+            call_noarg(self._clear_rl_specific)
         self.cache.c_clear_selected(whitelist = self.specific_cache_whitelist, blacklist = [])
+
+    cdef inline NvVector2 c_get_actual_size(self):
+        if self.get_param_value("strategy") == Strategy.Relative:
+            return self.size * self._resize_ratio
+        return self.size
+    
+    def get_actual_size(self):
+        return self.c_get_actual_size()        
+
+#=== Update functions ===
+    #========= UPDATE STRUCTURE: ==========
+    #    update >
+    #
+    #        primary_update >
+    #            logic_update >
+    #                all math and logic code
+    #            animation_update >
+    #                system animation code
+    #            event_update >
+    #                all pygame.event dependent code
+    #
+    #        secondary_update >
+    #            widget/layout update code
+    #
+    #        Update event cycle
+    #======================================
+
+    def _event_cycle(self, type, *args, **kwargs):
+        cdef NvParam events = self.get_param_strict("events")
+        cdef list content = events.value.content
+        self._core_event_cycle(type, content, args, kwargs)
+
+    cdef inline void _core_event_cycle(self, object type, list content, tuple args, dict kwargs):
+        cdef Py_ssize_t n = PyList_GET_SIZE(content)
+        cdef Py_ssize_t i = 0
+
+        while i < n:
+            event = <object>PyList_GET_ITEM(content, i)
+            if event._type == type:
+                event(*args, **kwargs)
+            i += 1
+    
+    cdef inline void _core_event_cycle_clear(self, object type):
+        cdef NvParam events = self.get_param_strict("events")
+        cdef list content = events.value.content
+        cdef Py_ssize_t n = PyList_GET_SIZE(content)
+        cdef Py_ssize_t i = 0
+        while i < n:
+            event = <object>PyList_GET_ITEM(content, i)
+            if event._type == type:
+                call_noarg(event)
+            i += 1
+
+    cpdef update(self):
+        events = nevu_state.current_events
+        if not self._active or not self._visible: return
+        self._primary_update(events)
+        if self._custom_secondary_update:
+            call_noarg(self.secondary_update)
+        self._core_event_cycle_clear(EventType.Update)
+
+    cdef inline void _primary_update(self, events):
+        events = events or []
+        self._base_logic_update()
+        if self._custom_logic_update:
+            call_noarg(self._logic_update)
+        self._base_animation_update()
+        if self._custom_animation_update:
+            call_noarg(self._animation_update)
+        if self._custom_event_update:
+            self._event_update(events)
+    
+    cdef inline void _base_animation_update(self):
+        self.animation_manager.update()
+
+    cdef inline void _base_logic_update(self):
+        if not self._sended_z_link and nevu_state.window != None:
+            self._sended_z_link = True
+            self._z_request = ZRequest.new(
+                link=self,
+                on_hover_func=self._hover, 
+                on_unhover_func=self._unhover,
+                on_scroll_func=self._group_on_scroll,
+                on_keyup_func=self._kup,
+                on_keyup_abandon_func=self._kup_abandon,
+                on_click_func=self._click)
+            nevu_state.window.add_request(self._z_request) # type: ignore
+        cdef list next_frame_functions = self._next_frame_functions
+        cdef Py_ssize_t n = PyList_GET_SIZE(next_frame_functions)
+        if n == 0: return
+        cdef Py_ssize_t i = 0
+        while i < n:
+            func = <object>PyList_GET_ITEM(next_frame_functions, i)
+            call_noarg(func)
+            i+=1
+        next_frame_functions.clear()
+
+
+#=== Draw functions ===
+    #========== DRAW STRUCTURE: ===========
+    #    draw >
+    #        primary_draw >
+    #            basic draw code
+    #
+    #        Draw event cycle
+    #
+    #        secondary_draw >
+    #            secondary_draw_content >
+    #                all additional draw | on change code
+    #            secondary_draw_end >
+    #                all after change code
+    #
+    #        Render event cycle
+    #======================================
+
+    cpdef draw(self):
+        if self._changed and self._active and self._visible and not self._wait_mode:
+            call_noarg(self.on_change)
+            self._on_change_system()
+        if self._custom_primary_draw:
+            call_noarg(self._primary_draw)
+        ce = self._core_event_cycle_clear
+        ce(EventType.Draw)
+        self._base_secondary_draw()
+        if self._custom_secondary_draw:
+            call_noarg(self.secondary_draw)
+        ce(EventType.Render)
+    
+    cdef inline void _base_secondary_draw(self):
+        if self._custom_secondary_draw_content:
+            call_noarg(self.secondary_draw_content)
+        self._base_secondary_draw_end()
+        if self._custom_secondary_draw_end:
+            call_noarg(self._secondary_draw_end)
+    
+    cdef inline void _base_secondary_draw_end(self):
+        if self._changed: self._changed = False
+
+    def on_state_change(self, state: HoverState): pass
+    def _on_state_change_system(self, state: HoverState): pass
+
+    #=== System hooks ===
+    cpdef _on_click_system(self): self._event_cycle(EventType.OnKeyDown, self)
+    cpdef _on_hover_system(self): self._event_cycle(EventType.OnHover, self)
+    cpdef _on_keyup_system(self): self._event_cycle(EventType.OnKeyUp, self)
+    cpdef _on_keyup_abandon_system(self): self._event_cycle(EventType.OnKeyUpAbandon, self)
+    cpdef _on_unhover_system(self): self._event_cycle(EventType.OnUnhover, self)
+    cpdef _on_scroll_system(self, bint side): self._event_cycle(EventType.OnMouseScroll, self, side)
+    cpdef _on_change_system(self): self._event_cycle(EventType.OnChange, self)
+
+    #=== Group functions ===
+    cpdef _group_on_click(self):
+        self._on_click_system()
+        call_noarg(self.on_click)
+    cpdef _group_on_hover(self):
+        self._on_hover_system()
+        call_noarg(self.on_hover)
+    cpdef _group_on_keyup(self):
+        self._on_keyup_system()
+        call_noarg(self.on_keyup)
+    cpdef _group_on_keyup_abandon(self):
+        self._on_keyup_abandon_system()
+        call_noarg(self.on_keyup_abandon)
+    cpdef _group_on_unhover(self):
+        self._on_unhover_system()
+        call_noarg(self.on_unhover)
+    cpdef _group_on_scroll(self, bint side):
+        self._on_scroll_system(side)
+        self.on_scroll(side)
+
+    #=== Selection functions ===
+    cpdef _click(self):
+        self._force_state_set_continue = True
+        self.set_hover_state(HoverState.Clicked)
+    cpdef _unhover(self): self.set_hover_state(HoverState.NotHovered)
+    cpdef _hover(self): self.set_hover_state(HoverState.Hovered)
+    cpdef _kup(self):
+        self._is_kup = True
+        self._force_state_set_continue = True
+        self.set_hover_state(HoverState.Hovered)
+    cpdef _kup_abandon(self):
+        self._kup_abandoned = True
+        self._force_state_set_continue = True
+        self.set_hover_state(HoverState.NotHovered)
+    
+    cdef inline set_hover_state(self, value): 
+        if self._hover_state == value and not self._force_state_set_continue: return
+        self.on_state_change(value)
+        self._on_state_change_system(value)
+        
+        if self._force_state_set_continue: self._force_state_set_continue = False
+        self._hover_state = value
+        
+        self.style.mark_state(value)
+        
+        
+        if value == HoverState.Clicked: 
+            self._group_on_click()
+        elif value == HoverState.Hovered:
+            if self._is_kup:
+                self._group_on_keyup()
+                self._is_kup = False
+            else: self._group_on_hover()
+        elif value == HoverState.NotHovered:
+            if self._kup_abandoned:
+                self._group_on_keyup_abandon()
+                self._kup_abandoned = False
+            else: self._group_on_unhover()
+                
+        self.after_state_change()
+        self._after_state_change_system()
