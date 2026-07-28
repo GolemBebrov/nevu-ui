bbda5e72096522f0537323a498b32cb97380aa4c
GolemBebrov <bebrovgolem@gmail.com>
2026-07-28 20:03:41 +0300

    Minor fix in check

--- src/nevu_ui/fast/nvparam/nvparam.pyx
+++ src/nevu_ui/fast/nvparam/nvparam.pyx
@@ -1,5 +1,6 @@
 # distutils: language = c++
 import weakref
+import typing
 cdef class NvParam:
     @staticmethod
     cdef NvParam new(str name, int layer, object value, object default, object type, object getter, object setter):
@@ -21,51 +22,53 @@ cdef class NvParam:
         self.type = type
         self.getter = getter
         self.setter = setter
-    
+
     cpdef bool check(self, value):
+        if self.type == typing.Any: return True
+
         return isinstance(value, self.type)
-    
+
     cpdef void set(self, value):
         cdef object new_value
         cdef object actual_setter
-        if self.setter != None: 
+        if self.setter != None:
             actual_setter = self.setter
 
             if isinstance(actual_setter, (weakref.ReferenceType, weakref.WeakMethod)) or type(actual_setter).__name__ in ('ref', 'WeakMethod'):
                 actual_setter = actual_setter()
-                
+
             if actual_setter != None:
-                
+
                 new_value = actual_setter(value)
                 if new_value != None:
                     self.value = new_value
                     return
-                    
+
         if not self.check(value):
             raise TypeError(f"Parameter '{self.name}' must be of type '{self._get_cool_error_message()}', but got '{value} ({type(value).__name__})'.")
         self.value = value
-        
+
     cpdef object get(self):
         cdef object actual_getter
-        
-        if self.getter != None: 
+
+        if self.getter != None:
             actual_getter = self.getter
             if isinstance(actual_getter, (weakref.ReferenceType, weakref.WeakMethod)) or type(actual_getter).__name__ in ('ref', 'WeakMethod'):
                 actual_getter = actual_getter()
-                
+
             if actual_getter != None:
                 return actual_getter(self.value)
-                
+
         return self.value
 
     def __repr__(self) -> str:
         return f"NvParam(name={self.name}, layer={self.layer}, value={self.value}, default={self.default}, type={self.type}, getter={self.getter}, setter={self.setter})"
-    
+
     def __str__(self) -> str:
         return f"{self.name}: {self.value}"
-    
+
     def _get_cool_error_message(self):
         return f"{self.type.__name__ if not isinstance(self.type, tuple) else ', '.join([t.__name__ for t in self.type])}"
 
     cpdef void reset(self):
-        self.value = self.default 
+        self.value = self.default
