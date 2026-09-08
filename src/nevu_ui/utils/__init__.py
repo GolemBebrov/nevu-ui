9c4bc9cb66b8c046edc830bf839faea4491d6967
GolemBebrov <bebrovgolem@gmail.com>
2026-09-08 17:47:40 +0300

    yes

--- src/nevu_ui/utils/__init__.py
+++ src/nevu_ui/utils/__init__.py
@@ -1,9 +1,18 @@
-from .mouse import mouse, set_mouse
-from .time import Time, time
-from .keyboard import  keyboard, set_keyboard
 from .input_type import InputType
-from .raylib_utills import load_font
+from .keyboard import keyboard, set_keyboard
 from .keys import Keys
+from .mouse import mouse, set_mouse
+from .raylib_utills import load_font
+from .time import Time, time
+
 __all__ = [
-    'Time', 'time', 'keyboard', 'InputType', 'mouse', 'set_keyboard', 'set_mouse', 'load_font', 'Keys'
+    'InputType',
+    'Keys',
+    'Time',
+    'keyboard',
+    'load_font',
+    'mouse',
+    'set_keyboard',
+    'set_mouse',
+    'time'
 ]
